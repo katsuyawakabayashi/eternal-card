4eae16d278742f5505b25ff05bb1a63e172ebe71
Katsuya Wakabayashi <wakabayashi@ucla.edu>
2022-04-19 10:37:13 -0700

    Make footer absolute positioning

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,28 +3,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="flex h-16 w-full items-center justify-around bg-black text-eternal-gray">
-        <div>&copy; {new Date().getFullYear()} ETERNAL CARD</div>
-        <div className="flex items-center gap-3 ">
-          <Link href="/about">About</Link>
-          <a
-            href="https://twitter.com/eternalcard"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>
-          <a
-            href="https://hexdom.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hexdom
-          </a>
-        </div>
-      </footer>
-    </div>
+    <footer className="absolute bottom-0 flex h-16 w-full items-center justify-around bg-black text-eternal-gray">
+      <div>&copy; {new Date().getFullYear()} ETERNAL CARD</div>
+      <div className="flex items-center gap-3 ">
+        <Link href="/about">About</Link>
+        <a
+          href="https://twitter.com/eternalcard"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a href="https://hexdom.com" target="_blank" rel="noopener noreferrer">
+          Hexdom
+        </a>
+      </div>
+    </footer>
   )
 }
 
